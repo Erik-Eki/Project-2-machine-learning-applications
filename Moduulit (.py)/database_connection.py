18b1f8200a8e031835886e70b86163165c7ef5e1
Valtteri Alastalo <valtterialastalo@kamk.fi>
2020-11-10 12:03:12 +0000

    Update main.ipynb and add database_query to modules

---
+++ Moduulit (.py)/database_connection.py
@@ -0,0 +1,17 @@
+def database_query(sql_query):
+    import pandas as pd
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+      host="172.28.200.50",
+      user="root",
+      port=3306,
+      passwd="insert-password-here",
+      database="iiwari_org")
+
+
+    mycursor = mydb.cursor(dictionary=True)
+    mycursor.execute(sql_query);
+
+    df = pd.DataFrame(mycursor.fetchall())
+    return df
