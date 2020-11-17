219fe84a98d5d6937bbbbd68323c419a7cf4147f
Erik <erikhuuskonen@kamk.fi>
2020-11-17 19:48:34 +0000

    poistettu yksi funktio clean_velocitiesesta

--- Moduulit/clean_velocities.py
+++ Moduulit/clean_velocities.py
@@ -1,6 +1,6 @@
 import numpy as np
-from datetime import datetime
 import matplotlib.pyplot as plt
+
 
 class velocity():
     # Nopeuden laskun funktio
@@ -19,21 +19,21 @@ class velocity():
         else:
             return 0.1
 
-    def x_vel(df):
+    def column_vel(df, column):
         # Alustaa muuttujia
         prev = 0
         val = 0
         x = 0
         # x kolumnin indexi
-        column = df.columns.get_loc('x')
+        column = df.columns.get_loc(column)
         # timestampin indexi
         time_column = df.columns.get_loc('timestamp')
         # Iteroidaan taulukon pituuden läpi...
-        for i in range(len(df['x'])):
+        for i in range(len(df[column])):
             # ...Niin pitkään kunnes päästään loppuun
-            if(i < len(df['x'])):
+            if(i < len(df[column])):
                 # Ottaa timestamp kolumnista yhden ja sitä seuraavan arvon ja laskee niiden välisen nopeuden
-                value1 = calc_velocity(df.iloc[i-x, time_column], df.iloc[i-(1+x), time_column])
+                value1 = velocity.calc_velocity(df.iloc[i-x, time_column], df.iloc[i-(1+x), time_column])
                 # Lasketaan absoluuttinen arvo ja vähennetään siitä edellisen nopeuden absoluuttinen arvo
                 value2 = int((abs(df.iloc[i-x, column])) - prev)
                 # Laskee näiden osamäärän
@@ -41,13 +41,13 @@ class velocity():
 
                 # Jos nopeus on liian suuri, pudottaa sen
                 if (val > 60 or value2 > 100):
-                    df1.drop([df.index[i-x]], axis = 0, inplace = True)
+                    df.drop([df.index[i-x]], axis = 0, inplace = True)
                     prev = abs(df.iloc[i-x, column])
                     x +=1
                 else:
                     prev = abs(df.iloc[i-x, column])
 
-    def y_vel(df):
+    '''def y_vel(df, column):
         prev = 0
         val = 0
         x = 0
@@ -57,7 +57,7 @@ class velocity():
 
             if(i < len(df['y'])):
 
-                value1 = calc_velocity(df.iloc[i-x, time_column], df.iloc[i-(1+x), time_column])
+                value1 = velocity.calc_velocity(df.iloc[i-x, time_column], df.iloc[i-(1+x), time_column])
                 value2 = int((abs(df.iloc[i-x, column]))-prev)
                 val = value2 / value1
 
@@ -66,14 +66,13 @@ class velocity():
                     prev = abs(df.iloc[i-x , column])
                     x +=1
                 else:
-                    prev = abs(df.iloc[i-x, column])
+                    prev = abs(df.iloc[i-x, column])'''
 
 
-    def draw_vel():
+    def draw_vel(df_original, df_new, columnX, columnY):
         plt.figure(figsize=(10, 7))
-        plt.plot(df_original['x_grid'], df_original['y_grid'], color="black", marker='o', linestyle='dashed', linewidth=0.2, markersize=3, label="Poistettu")
-        #plt.plot(df1['x'], df1['y'], color="cyan", marker='o', linestyle='dashed', outline="r" linewidth=0.2, markersize=3, alpha=0.2, label="Poistettud")
-        plt.plot(df_new['x_grid'], df_new['y_grid'],color='cyan', marker='o',linewidth=0.2, markersize=2, markevery=3, label="Jääneet", alpha=0.3)
+        plt.plot(df_original[columnX], df_original[columnY], color="black", marker='o', linestyle='dashed', linewidth=0.2, markersize=3, label="Poistettu")
+        plt.plot(df_new[columnX], df_new[columnY],color='cyan', marker='o',linewidth=0.2, markersize=2, markevery=3, label="Jääneet", alpha=0.3)
         plt.title("Liiat nopeudet poistettu")
         plt.legend()
         plt.show()
