ef6cca120426ce87bb3e17be0aef2f26f3f78814
juha vartiainen <juhavartiainen1@kamk.fi>
2020-11-25 14:27:46 +0000

    Diskretisointi paivitetty.
    Former-commit-id: 135e74273d91f433cbbc06bdf9bbaa757fb86a4f

--- Moduulit/xy_diskretisointi.py
+++ Moduulit/xy_diskretisointi.py
@@ -1,25 +1,10 @@
 import pandas as pd
 import math
-
 def xy_to_grid(df,x,y,k):
-    
-    df = df.reset_index(drop=True)
-    
-    xmin, ymin = df.x.min(), df.y.min()
-    xmax, ymax = df.x.max(), df.y.max()
-    gridlista_x, gridlista_y = [], []
-
-    for i in range(len(df)):
-        gridlista_x.append(math.floor((k-1)*(df.loc[i].x-xmin)/(xmax-xmin)))
-        gridlista_y.append(math.floor((k-1)*(df.loc[i].y-ymin)/(ymax-ymin)))
-
-    
-    # Laitetaan saadut liukuluvut dataframeen
-    df['x_grid'],df['y_grid'] = gridlista_x, gridlista_y
-
+    x_cut = pd.cut(df.x, k,labels=False)
+    y_cut = pd.cut(df.y, k,labels=False)
+    # Laitetaan saadut arvot dataframeen
+    df['x_grid'], df['y_grid'] = x_cut.values, y_cut.values
     # Laitetaan arvot dataframeen, xygridiID = y * gridsize +x
     df['grid_id'] = df['y_grid'] * k + df['x_grid']
-
-    df = df.reset_index(drop=True)
-    
     return df
