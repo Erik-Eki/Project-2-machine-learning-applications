aeb7ff91576a54c035c5b14eb30d347248858273
Erik <erikhuuskonen@kamk.fi>
2020-11-26 09:12:16 +0000

    modulaarisuutta sisään-ulos.py
    Former-commit-id: e25b465413184f6c29464f0a4ca61cb7abd1201c

--- Moduulit/sisään_uloskäynnit.py
+++ Moduulit/sisään_uloskäynnit.py
@@ -1,32 +1,34 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-def sisään(df):
-    grid_size = max(df.x_grid)+1
-    min_x = 0.6 * grid_size
+def sisään(df, x, y):
+    grid_size = max(df[x])+1
+    min_x = 0.7 * grid_size
     max_x = 0.8 * grid_size
     min_y = 0 * grid_size
     max_y = 0.35 * grid_size
     
-    sisään_x = df.loc[df.x_grid>=min_x].loc[df.x_grid<=max_x].loc[df.y_grid>=min_y].loc[df.y_grid<=max_y].x_grid
-    sisään_y = df.loc[df.x_grid>=min_x].loc[df.x_grid<=max_x].loc[df.y_grid>=min_y].loc[df.y_grid<=max_y].y_grid
+    sisään_x = df.loc[df[x]>=min_x].loc[df[x]<=max_x].loc[df[y]>=min_y].loc[df[y]<=max_y][x]
+    sisään_y = df.loc[df[x]>=min_x].loc[df[x]<=max_x].loc[df[y]>=min_y].loc[df[y]<=max_y][y]
     
     return sisään_x, sisään_y
 
-def ulos(df):
-    grid_size = max(df.y_grid)+1
+def ulos(df, x, y):
+    grid_size = max(df[y])+1
     min_x = 0.3 * grid_size
-    max_x = 0.5 * grid_size
+    max_x = 0.55 * grid_size
     min_y = 0.2 * grid_size
     max_y = 0.35 * grid_size
     
-    ulos_x = df.loc[df.x_grid>=min_x].loc[df.x_grid<=max_x].loc[df.y_grid>=min_y].loc[df.y_grid<=max_y].x_grid
-    ulos_y = df.loc[df.x_grid>=min_x].loc[df.x_grid<=max_x].loc[df.y_grid>=min_y].loc[df.y_grid<=max_y].y_grid
+    #ulos_x = df.loc[df.x_grid>=min_x].loc[df.x_grid<=max_x].loc[df.y_grid>=min_y].loc[df.y_grid<=max_y].x_grid
+    #ulos_y = df.loc[df.x_grid>=min_x].loc[df.x_grid<=max_x].loc[df.y_grid>=min_y].loc[df.y_grid<=max_y].y_grid
+    ulos_x = df.loc[df[x]>=min_x].loc[df[x]<=max_x].loc[df[y]>=min_y].loc[df[y]<=max_y][x]
+    ulos_y = df.loc[df[x]>=min_x].loc[df[x]<=max_x].loc[df[y]>=min_y].loc[df[y]<=max_y][y]
     
     return ulos_x, ulos_y
 
-def draw_exits(df, sisään_x, sisään_y, ulos_x, ulos_y):
-    plt.hist2d(df['x_grid'], df['y_grid'], bins = 100, norm = colors.LogNorm())#bins=[np.arange(0,400,5),np.arange(0,300,5)]
+def draw_exits(df, sisään_x, sisään_y, ulos_x, ulos_y, x, y):
+    plt.hist2d(df[x], df[y], bins = 100, norm = colors.LogNorm())#bins=[np.arange(0,400,5),np.arange(0,300,5)]
 
     plt.hist2d(sisään_x, sisään_y, bins = 150, norm=colors.LogNorm(),cmap="cool", label="Sisäänkäynti")
     plt.hist2d(ulos_x, ulos_y, bins = 150, norm=colors.LogNorm(),cmap="spring", label="Kassat")
