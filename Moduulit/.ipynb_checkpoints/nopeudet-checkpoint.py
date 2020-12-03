b7a0d88e69e78d09b9caced0d17311243b3fd3d0
LauriPel1 <lauripellinen1@kamk.fi>
2020-12-03 07:06:43 +0000

    vitunnopee
    Former-commit-id: a20af0d382f203fd1155cfdde85b0e1252124d18

--- Moduulit/.ipynb_checkpoints/nopeudet-checkpoint.py
+++ Moduulit/.ipynb_checkpoints/nopeudet-checkpoint.py
@@ -1,6 +1,31 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 4
-}
+import pandas as pd
+import numpy as np
+
+def nopeudet(df):
+    
+
+    df['distancex'] = df['x'].diff()
+    df['distancey'] = df['y'].diff()
+    df['distance'] = (df['distancex']**2 + df['distancey']**2)
+    df['distance'] = (np.sqrt(df['distance'])/161.15)
+
+    df = df.drop('distancex', 1)
+    df = df.drop('distancey', 1)
+
+
+#df['distance'] = ((np.sqrt((df['x'] - df['x'].shift(-1))**2 + (df['y'] - df['y'].shift(-1))**2))/161.15)
+#df['distance'] = ((np.sqrt((df['x'].diff()**2 + (df['y'].diff()**2))/161.15)))
+
+
+    df['ero'] = df['timestamp'].diff()
+    df['ero'] = df.ero.dt.seconds                   
+                   
+    df['speedkm'] = df['distance']/df['ero']*3.6
+
+
+    # Poistetaan liian nopeat, yli 7km/h
+    df = df.drop(df[(df.speedkm > 7)].index)
+    
+    df = df.dropna()
+    df
+    
