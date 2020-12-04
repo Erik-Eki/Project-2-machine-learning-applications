bcbaa914764eff7d70973223223cd72f15fb75e6
“Erik <erikhuuskonen@kamk.fi>
2020-12-04 09:47:13 +0200

    Update nopeudet.py
    Former-commit-id: a1e0633a903f5a798ae7416527ccc7fb97b37fad

--- Moduulit/nopeudet.py
+++ Moduulit/nopeudet.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def nopeudet(df):
     
-
+    df_original = df.copy()
     df['distancex'] = df['x'].diff()
     df['distancey'] = df['y'].diff()
     df['distance'] = (df['distancex']**2 + df['distancey']**2)
@@ -13,10 +13,6 @@ def nopeudet(df):
     df = df.drop('distancey', 1)
 
 
-#df['distance'] = ((np.sqrt((df['x'] - df['x'].shift(-1))**2 + (df['y'] - df['y'].shift(-1))**2))/161.15)
-#df['distance'] = ((np.sqrt((df['x'].diff()**2 + (df['y'].diff()**2))/161.15)))
-
-
     df['ero'] = df['timestamp'].diff()
     df['ero'] = df.ero.dt.seconds                   
                    
@@ -24,8 +20,18 @@ def nopeudet(df):
 
 
     # Poistetaan liian nopeat, yli 7km/h
-#     df = df.drop(df[(df.speedkm > 7)].index)
     
     df = df.dropna()
-    df
     
+    print("Vanha taulu: ", len(df_original))
+    print("Uusi taulu: ", len(df['x']))
+    print("Poistettuja pisteitä: ", len(df_original) - len(df))
+    total_data = len(df_original)
+    total_missing = len(df_original) - len(df)
+    percentage = (total_missing/total_data) * 100
+    percentage_remain = (1 - (total_missing/total_data)) * 100
+    print("Percent removed:   ",round(percentage, 2),'%')
+    print("Percent remaining: ",round(percentage_remain, 2),'%')
+    print(f"{'-'*30}")
+    
+    return df
